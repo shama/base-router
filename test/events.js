8abbeac015108cb4c586d1bc1562a52b1465c484
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2015-06-03 00:15:03 -0400

    Test window.history push and pop state

--- test/events.js
+++ test/events.js
@@ -1,8 +1,9 @@
 var Router = require('../')
 var test = require('tape')
 var location = require('global/window').location
+var history = require('global/window').history
 
-if (location) {
+if (location && history) {
   test('hashchange', function (t) {
     t.plan(6)
     var first = true
@@ -26,5 +27,35 @@ if (location) {
     })
     location.hash = '#/one'
   })
+
+  test('history', function (t) {
+    t.plan(6)
+    var first = true
+    var second, back, forward = false
+    var router = new Router({
+      '/one': function () { return 1 },
+      '/two': function () { return 2 }
+    }, {location: 'history'})
+    router.on('transition', function (route, data) {
+      if (first) {
+        first = false; second = true
+        t.equal(route, '/one')
+        t.equal(data, 1)
+        router.transitionTo('/two')
+      } else if (second) {
+        second = false; back = true
+        history.back()
+      } else if (back) {
+        back = false; forward = true
+        t.equal(route, '/one')
+        t.equal(data, 1)
+        history.go(1)
+      } else if (forward) {
+        t.equal(route, '/two')
+        t.equal(data, 2)
+        t.end()
+      }
+    })
+    router.transitionTo('/one')
+  })
 }
-module.exports = {}
